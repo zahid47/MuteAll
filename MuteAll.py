845158f59ae765546802bf32c339d56288a65393
scarecow <epiczahid@gmail.com>
2020-10-24 20:45:54 +0600

    added 2 new experimental mute and un-mute commands + other QOL improvements

--- MuteAll.py
+++ MuteAll.py
@@ -2,17 +2,16 @@ import discord
 from discord.ext import commands
 import os
 
-
+# For local testing only
 # with open("TOKEN.txt") as file:
 #     TOKEN = file.read()
-
 
 TOKEN = os.environ["TOKEN"]
 
 client = commands.Bot(command_prefix=".")
 
 
-# send a msg when the bot is ready and set status
+# sets status when the bot is ready
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(".mute | .unmute"))
@@ -23,7 +22,7 @@ async def on_ready():
 client.remove_command("help")
 
 
-# show help text
+# shows help text
 @client.command(aliases=["commands", "Help", "h", "H"])
 async def help(ctx):
     embed = discord.Embed()
@@ -31,69 +30,136 @@ async def help(ctx):
     embed.add_field(name="`.ping`", value="Latency of the bot", inline=False)
     embed.add_field(name="`.mute` / `.m`", value="Mutes everyone in your current voice channel, both you and the bot "
                                                  "require `Mute Members` permission", inline=False)
-    embed.add_field(name="`.unmute` / `.u`", value="Unmutes everyone in your current voice channel, only the bot "
+    embed.add_field(name="`.unmute` / `.u`", value="Un-mutes everyone in your current voice channel, only the bot "
                                                    "requires `Mute Members` permission", inline=False)
+    embed.add_field(name="`.xm`", value="Experimental mute (useful if you use a music bot). Mutes humans and un-mutes "
+                                        "bots in your current voice channel. Both you and the bot require `Mute "
+                                        "Members` permission", inline=False)
+    embed.add_field(name="`.xu`",
+                    value="Experimental un-mute (useful if you use a music bot). Un-mutes humans and mutes "
+                          "bots in your current voice channel. Only the bot requires `Mute Members` permission",
+                    inline=False)
     embed.add_field(name="_", value="[Join support server](https://discord.com/invite/Jxv66vm)", inline=False)
 
     await ctx.send(embed=embed)
 
 
-# get ping of the bot
+# shows latency of the bot
 @client.command(aliases=["latency"])
 async def ping(ctx):
     await ctx.send(f"ping {round(client.latency * 1000)} ms")
 
 
-# muting everyone in the current voice channel
-# TODO only mute someone if they are not already muted
-@client.command(aliases=["m", "stfu", "M", "Mute"])
+# mutes everyone (except for bots) in the current voice channel
+@client.command(aliases=["m", "M", "Mute"])
 async def mute(ctx):
     try:
-        if ctx.author.voice:  # its true if the user is connected to a voice channel
-            try:
-                if ctx.author.guild_permissions.mute_members:  # its true if the user has mute_members permission
+        if ctx.author.voice:  # check if the user is in a voice channel
+            if ctx.author.guild_permissions.mute_members:  # check if the user has mute members permission
+                try:  # try to mute if the bot has permissions
                     no_of_members = 0
-                    # muted_members = []
-                    for member in ctx.author.voice.channel.members:
-                        await member.edit(mute=True)
-                        no_of_members += 1
-                        # muted_members.append(member.name)
-                    await ctx.channel.send(f"Muted {no_of_members} user(s) in {ctx.author.voice.channel}.")
-                else:
-                    await ctx.channel.send("You don't have the `Mute Members` permission.")
-            except discord.errors.Forbidden:
-                await ctx.channel.send(f"I don't have the `Mute Members` permission. Please grant me the permission "
-                                       f"in my role **and** in your "
-                                       f"current voice channel `{ctx.author.voice.channel}`")
+                    for member in ctx.author.voice.channel.members:  # traverse through the members list in current vc
+                        if not member.bot:  # check if member is a bot, don't mute if bot
+                            await member.edit(mute=True)  # finally muting the member
+                            no_of_members += 1
+                    if no_of_members < 2:
+                        await ctx.channel.send(f"Muted {no_of_members} user in {ctx.author.voice.channel}")
+                    else:
+                        await ctx.channel.send(f"Muted {no_of_members} users in {ctx.author.voice.channel}")
+                except discord.errors.Forbidden:
+                    await ctx.channel.send(  # the bot doesn't have the permission to mute
+                        f"I don't have the `Mute Members` permission. Make sure I have the permission in my role "
+                        f"**and** in your current voice channel `{ctx.author.voice.channel}`")
+            else:
+                await ctx.channel.send("You don't have the `Mute Members` permission")
         else:
-            await ctx.channel.send("You must be in a voice channel first.")
+            await ctx.send("You must join a voice channel first")
     except Exception as e:
-        await ctx.channel.send(f"Something went wrong ({e}) I'm still in testing phase. Please contact my sensei "
-                               "on discord `SCARECOW#0456`")
+        await ctx.channel.send(f"Something went wrong ({e}). Please contact my sensei`SCARECOW#0456`")
 
 
-# un-muting everyone in the current voice channel
+# [experimental mute] mutes everyone in the current voice channel and mutes the bots, useful for music bots!
+@client.command(aliases=["Xm"])
+async def xm(ctx):
+    try:
+        if ctx.author.voice:  # check if the user is in a voice channel
+            if ctx.author.guild_permissions.mute_members:  # check if the user has mute members permission
+                try:  # try to mute if the bot has permissions
+                    no_of_members = 0
+                    for member in ctx.author.voice.channel.members:  # traverse through the members list in current vc
+                        if not member.bot:  # check if member is not a bot
+                            await member.edit(mute=True)  # mute the non-bot member
+                            no_of_members += 1
+                        else:
+                            await member.edit(mute=False)  # un-mute the bot member
+                            await ctx.send(f"Un-muted {member.name}")
+                    if no_of_members < 2:
+                        await ctx.channel.send(f"Un-muted {no_of_members} user in {ctx.author.voice.channel}")
+                    else:
+                        await ctx.channel.send(f"Un-muted {no_of_members} users in {ctx.author.voice.channel}")
+                except discord.errors.Forbidden:
+                    await ctx.channel.send(  # the bot doesn't have the permission to mute
+                        f"I don't have the `Mute Members` permission. Make sure I have the permission in my role "
+                        f"**and** in your current voice channel `{ctx.author.voice.channel}`")
+            else:
+                await ctx.channel.send("You don't have the `Mute Members` permission")
+        else:
+            await ctx.send("You must join a voice channel first")
+    except Exception as e:
+        await ctx.channel.send(f"Something went wrong ({e}). Please contact my sensei`SCARECOW#0456`")
+
+
+# un-mutes everyone (except for bots) in the current voice channel
 @client.command(aliases=["um", "un", "un-mute", "u", "U", "Un", "Um", "Unmute"])
 async def unmute(ctx):
     try:
-        if ctx.author.voice:  # this will be true if the user is connected to a voice channel
-            try:
+        if ctx.author.voice:  # check if the user is in a voice channel
+            try:  # try to un-mute if the bot has permissions
                 no_of_members = 0
-                # muted_members = []
-                for member in ctx.author.voice.channel.members:
-                    await member.edit(mute=False)
-                    no_of_members += 1
-                    # muted_members.append(member.name)
-                await ctx.channel.send(f"Un-muted {no_of_members} user(s) in {ctx.author.voice.channel}.")
+                for member in ctx.author.voice.channel.members:  # traverse through the members list in current vc
+                    if not member.bot:  # check if member is a bot, don't un-mute if bot
+                        await member.edit(mute=False)  # finally un-muting the member
+                        no_of_members += 1
+                if no_of_members < 2:
+                    await ctx.channel.send(f"Un-muted {no_of_members} user in {ctx.author.voice.channel}")
+                else:
+                    await ctx.channel.send(f"Un-muted {no_of_members} users in {ctx.author.voice.channel}")
             except discord.errors.Forbidden:
-                await ctx.channel.send(f"I don't have the `Mute Members` permission. Please grant me the permission "
-                                       f"in my role **and** in your "
-                                       f"current voice channel `{ctx.author.voice.channel}`")
+                await ctx.channel.send(  # the bot doesn't have the permission to mute
+                    f"I don't have the `Mute Members` permission. Make sure I have the permission in my role "
+                    f"**and** in your current voice channel `{ctx.author.voice.channel}`")
         else:
-            await ctx.channel.send("You must be in a voice channel first.")
+            await ctx.send("You must join a voice channel first")
     except Exception as e:
-        await ctx.channel.send(f"Something went wrong ({e}) I'm still in testing phase. Please contact my sensei "
-                               "on discord `SCARECOW#0456`")
+        await ctx.channel.send(f"Something went wrong ({e}). Please contact my sensei`SCARECOW#0456`")
+
+
+# [experimental un-mute] un-mutes everyone in the current voice channel and mutes the bots, useful for music bots!
+@client.command(aliases=["Xu"])
+async def xu(ctx):
+    try:
+        if ctx.author.voice:  # check if the user is in a voice channel
+            try:  # try to un-mute if the bot has permissions
+                no_of_members = 0
+                for member in ctx.author.voice.channel.members:  # traverse through the members list in current vc
+                    if not member.bot:  # check if member is not a bot
+                        await member.edit(mute=False)  # un-mute the non-bot member
+                        no_of_members += 1
+                    else:
+                        await member.edit(mute=True)  # mute the bot member
+                        await ctx.send(f"Muted {member.name}")
+                if no_of_members < 2:
+                    await ctx.channel.send(f"Un-muted {no_of_members} user in {ctx.author.voice.channel}")
+                else:
+                    await ctx.channel.send(f"Un-muted {no_of_members} users in {ctx.author.voice.channel}")
+            except discord.errors.Forbidden:
+                await ctx.channel.send(  # the bot doesn't have the permission to mute
+                    f"I don't have the `Mute Members` permission. Make sure I have the permission in my role "
+                    f"**and** in your current voice channel `{ctx.author.voice.channel}`")
+        else:
+            await ctx.send("You must join a voice channel first")
+    except Exception as e:
+        await ctx.channel.send(f"Something went wrong ({e}). Please contact my sensei`SCARECOW#0456`")
 
 
 # run the bot
