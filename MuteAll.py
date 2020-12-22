3020f63ea7029f67673f47c73c34d1e61cae7021
zahid47 <epiczahid@gmail.com>
2020-12-22 00:41:58 -0800

    Update MuteAll.py

--- MuteAll.py
+++ MuteAll.py
@@ -14,7 +14,7 @@ client.remove_command("help")
 # sets status when the bot is ready
 @client.event
 async def on_ready():
-    activity = discord.Activity(name="Try the new role MINION! Type .help for more info.", type=discord.ActivityType.playing)
+    activity = discord.Activity(name="with the new minion role! Type .help for more info.", type=discord.ActivityType.playing)
     await client.change_presence(status=discord.Status.online, activity=activity)
     print("Ready!")
 
